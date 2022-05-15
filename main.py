00df0776a559bd7745416b63f11356d26c20ba7a
sonya-c <sonya.castro.gomez@gmail.com>
2022-05-14 23:28:12 -0500

    add: heroku confi

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+import os
+
 # Telegram stuff
 from telegram.ext.updater import Updater
 from telegram.update import Update
@@ -12,16 +15,16 @@ from cesar import encode, decode
 
 
 def start(update: Update, context: CallbackContext):
-  update.message.reply_text("Hola, Soy un Robot \U0001F916")
+    update.message.reply_text("Hola, Soy un Robot \U0001F916")
 
 
 def content(update: Update, context: CallbackContext):
-  update.message.reply_text(
-    """Tengo un robot, yo lo hago funcionar, \n él es el mejor y vamos a jugar.""")
+    update.message.reply_text(
+        """Tengo un robot, yo lo hago funcionar, \n él es el mejor y vamos a jugar.""")
 
 
 def contact(update: Update, context: CallbackContext):
-  update.message.reply_text("""
+    update.message.reply_text("""
   BOT CREADO POR:
   Líder: 
     Nombre: Sonya Castro
@@ -30,10 +33,11 @@ def contact(update: Update, context: CallbackContext):
     Nombre: Natalia Mendonza
     Mail: npmendoza@uninorte.edu.co
     """
-  )
+                              )
+
 
 def help(update: Update, context: CallbackContext):
-  update.message.reply_text("""
+    update.message.reply_text("""
   COMANDOS:
   /start -> Bienvenida
   /help -> Menú
@@ -45,57 +49,66 @@ def help(update: Update, context: CallbackContext):
 
 
 def unknown_text(update: Update, context: CallbackContext):
-  update.message.reply_text(
-      "Lo siento, no pude reconocer lo que dijiste '%s'" % update.message.text)
+    update.message.reply_text(
+        "Lo siento, no pude reconocer lo que dijiste '%s'" % update.message.text)
 
 
 def unknown(update: Update, context: CallbackContext):
-  update.message.reply_text("Lo siento, '%s' no es un comando valido" % update.message.text)
+    update.message.reply_text(
+        "Lo siento, '%s' no es un comando valido" % update.message.text)
 
 
 def codificar(update: Update, context: CallbackContext):
-  text = (update.message.text).replace("/codificar ", "").replace("/c ", "")
+    text = (update.message.text).replace("/codificar ", "").replace("/c ", "")
 
-  encode_text, typo_words = encode(text)
-  
-  update.message.reply_text(encode_text)
+    encode_text, typo_words = encode(text)
+
+    update.message.reply_text(encode_text)
 
 
 def decodificar(update: Update, context: CallbackContext):
-  text = (update.message.text).replace("/decodificar ", "").replace("/d ", "")
+    text = (update.message.text).replace(
+        "/decodificar ", "").replace("/d ", "")
 
-  decode_text = decode(text)
-  update.message.reply_text(decode_text)
+    decode_text = decode(text)
+    update.message.reply_text(decode_text)
 
 
 def main():
+    PORT = int(os.environ.get('PORT', 5000))
+    print("Server started!")
 
-  print("Server started!")
+    # read file with the key gave by BotFather
+    with open('key.txt', 'r') as f:
+        KEY = f.read()
 
-  #read file with the key gave by BotFather
-  with open('key.txt', 'r') as f:
-      KEY = f.read()
-      
-  #Permite que el bot ande, usando como servidor local el computador
-  updater = Updater(KEY, use_context = True)
+    # Permite que el bot ande, usando como servidor local el computador
+    updater = Updater(KEY, use_context=True)
 
-  #para aceptar los comandos
-  disp = updater.dispatcher
+    # para aceptar los comandos
+    disp = updater.dispatcher
 
-  disp.add_handler(CommandHandler("start", start))
-  disp.add_handler(CommandHandler("content", content))
-  disp.add_handler(CommandHandler("help", help))
-  disp.add_handler(CommandHandler("contact", contact))
+    disp.add_handler(CommandHandler("start", start))
+    disp.add_handler(CommandHandler("content", content))
+    disp.add_handler(CommandHandler("help", help))
+    disp.add_handler(CommandHandler("contact", contact))
 
-  disp.add_handler(CommandHandler("c", codificar))
+    disp.add_handler(CommandHandler("c", codificar))
 
-  disp.add_handler(CommandHandler("d", decodificar))
+    disp.add_handler(CommandHandler("d", decodificar))
 
-  disp.add_handler(MessageHandler(Filters.text, unknown))
-  disp.add_handler(MessageHandler(Filters.command, unknown))
+    disp.add_handler(MessageHandler(Filters.text, unknown))
+    disp.add_handler(MessageHandler(Filters.command, unknown))
 
-  updater.start_polling()
-  updater.idle()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=KEY)
+
+    updater.bot.setWebhook('https://casmyn-bot.herokuapp.com/' + KEY)
+
+    # updater.start_polling()
+    updater.idle()
+
 
 if __name__ == '__main__':
-  main()
+    main()
